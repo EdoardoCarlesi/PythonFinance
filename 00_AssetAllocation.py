4c5bf3460274866d82ec030c574b1b29b50b2f2e
Edoardo <gatto@nanowar.it>
2020-11-20 00:53:19 +0100

    asset allocation

--- 00_AssetAllocation.py
+++ 00_AssetAllocation.py
@@ -11,6 +11,10 @@ from copy import copy
 
 
 def normalize(data=None):
+    """
+        Normalize all data to 1.0 at their starting values
+    """
+
     x = data.copy()
 
     for i in x.columns[1:]:
@@ -20,6 +24,9 @@ def normalize(data=None):
 
 
 def normalized_random(N):
+    """
+        Returns a list of N random numbers, ensuring the total is normalized to one
+    """
 
     numbers = [np.random.random() for i in range(0, N)]
     norm = sum(numbers)
@@ -29,7 +36,11 @@ def normalized_random(N):
 
 
 def make_portfolio(data=None, w=None):
-    
+    """
+        This function takes a normalized dataset as an input and returns a new dataframe 
+        whose columns are the different assets multiplied by the weights
+    """
+
     data_w = pd.DataFrame()
 
     for i, col in enumerate(data.columns[1:]):
@@ -38,12 +49,44 @@ def make_portfolio(data=None, w=None):
     return data_w
 
 
-def interactive_plot(data=None, title=None):
+def daily_returns(data=None, col=None):
+    """
+        Compute the daily returns of a single stock / portfolio
+    """
 
-    fig = px.line(title = title)
+    r = []
+
+    # Initialize to zero on the first day of counting
+    r.append(0.0)
+
+    for i in range(1, len(data)):
+        r.append((data[col][i] - data[col][i-1])/data[col][i-1])
+
+    return np.array(r)
+
+
+def cumulative_returns(data=None, col=None):
+    """
+        Compute the final total return of a single stock / portfolio
+    """
+
+    return (data[col][-1:] - data[col][0])/data[col][0]
+
+
+def interactive_plot(data=None, title=None, mode="lines"):
+    """
+        This is an interactive plot generator for the browser, using plotly
+    """
+
+    if mode == "lines":
+        fig = px.line(title = title)
     
-    for i in data.columns[1:]:
-        fig.add_trace(go.Scatter(x = data['Date'], y = data[i], mode="lines", name = i))
+        for col in data.columns[1:]:
+            fig.add_trace(go.Scatter(x = data['Date'], y = data[col], mode=mode, name = col))
+
+    # In histogram mode we assume data is already 1-dimensional
+    elif mode == "histogram":
+        fig = px.histogram(data, title = title)
 
     fig.show()
 
@@ -64,10 +107,21 @@ def montecarlo(data=None, n_runs=1):
         portfolio = make_portfolio(data=data, w=w)       
 
         key_p = 'P_' + str(i_mc)        
-        
         portfolios[key_p] = portfolio[data_cols].apply(lambda x: sum(x), axis = 1)
 
     return portfolios
+
+
+def sharpe_ratio(Rp=None, Rf=None, sigma=None):
+    """
+        The Sharpe ratio is defined as 
+        SR = (R_p - R_f) / sigma_p
+        R_p = return of the portfolio
+        R_f = risk free return
+        sigma_p = volatility (std dev) of my portfolio
+    """
+
+    return (Rp - Rf) / sigma
 
 
 if __name__ == "__main__":
@@ -86,12 +140,35 @@ if __name__ == "__main__":
     #interactive_plot(data = stock_norm, title = 'Normalized Prices')
     
     n_cols = len(stock_norm.columns) -1
-    n_runs_mc = 100
+    n_runs_mc = 3
 
     portfolios = montecarlo(data=stock_norm, n_runs=n_runs_mc)
-    print(portfolios.head())
+    #print(portfolios.head())
 
-    interactive_plot(data=portfolios, title='MC')
+    #interactive_plot(data=portfolios, title='MC')
+
+    for i, col in enumerate(portfolios.columns[1:]):
+        return_col = 'R_' + str(i)
+        portfolio_col = 'P_' + str(i)
+        portfolios[return_col] = daily_returns(data=portfolios, col=col)
+        
+        # Plot just one series of return
+        df_tmp = portfolios[['Date', return_col]]
+        #interactive_plot(data = df_tmp, title = 'Daily returns')
+
+        #interactive_plot(data=portfolios[new_col], mode="histogram", title="Returns")
+        rf = 0.00
+        rp = portfolios[return_col].mean()
+        sigma = portfolios[return_col].std()
+        cumret = cumulative_returns(portfolios, col=portfolio_col).values[0]
+        sr = sharpe_ratio(Rf=rf, Rp=rp, sigma=sigma)
+        sr *= np.sqrt(252) # Adjust to the whole year
+
+        print(f'Portfolio cumulative returns: {cumret}')
+        print(f'Portfolio standard deviation: {sigma}')
+        print(f'Portfolio avg. daily return : {rp}')
+        print(f'Sharpe ratio: {sr}')
+    
 
 
 
